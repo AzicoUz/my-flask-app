af53910d06bc76dbc22bbaeedf79b6bdef245c74
Azico.Uz <162594858+AzicoUz@users.noreply.github.com>
2025-06-08 09:19:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ if __name__ == "__main__":
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render portni shu tarzda beradi
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
